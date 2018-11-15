ecfed375cce663ef7269041f116c1d16f9d13d5c
Jonathan <jon1969edwards@gmail.com>
2018-11-15 19:57:23 +0100

    creates test in index spec for bio

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -8,5 +8,13 @@ describe 'Index Page', type: :feature do
             expect(page).to have_content 'My First Website'
             expect(page).to have_content 'FizzBuzz'
         end
+
+        expect(page).to have_css '.bio'
+
+        within '.bio' do
+            expect(page).to have_content 'American'
+            expect(page).to have_content '06/02/1977'
+            expect(page).to have_content 'Music, Record collection, Video Games, Technology'
+        end
     end
 end
