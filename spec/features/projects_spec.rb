5eeb3bb293430a45372fc9be324554ae85e3605a
Jonathan <jon1969edwards@gmail.com>
2018-11-16 15:29:54 +0100

    creates test and link to page for projects.  all tests green

---
+++ spec/features/projects_spec.rb
@@ -0,0 +1,12 @@
+describe 'Projects Page', type: :feature do
+    it 'displays project list' do
+        visit 'projects.html'
+
+        expect(page).to have_css '.projects'
+
+        within '.projects' do
+            expect(page).to have_content 'My First Website'
+            expect(page).to have_content 'FizzBuzz'
+        end
+    end
+end
