98a37bfd981d46a9112989b969c6b54d9842a127
Jonathan <jon1969edwards@gmail.com>
2018-12-21 14:10:54 +0100

    Links html to partials

--- spec/features/work_history_spec.rb
+++ spec/features/work_history_spec.rb
@@ -6,7 +6,13 @@ describe 'Work History', type: :feature do
 
         within '.work_history' do
             expect(page).to have_content 'Wirstroms Pub'
+            expect(page).to have_content 'Bartender'
+            expect(page).to have_content 'Stockholm'
+            expect(page).to have_content '3'
             expect(page).to have_content 'Idle Hour'
+            expect(page).to have_content 'Bartender'
+            expect(page).to have_content 'Sacramento, CA'
+            expect(page).to have_content '2'
         end
     end
 end
