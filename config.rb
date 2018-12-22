6e118e4c4d674ab1457e1113f1e3ab7f6686d89e
Jonathan <jon1969edwards@gmail.com>
2018-12-22 14:58:21 +0100

    Adds contact info and refactors index.spec

--- config.rb
+++ config.rb
@@ -13,6 +13,9 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+# Disable warnings
+Haml::TempleEngine.disable_option_validator!
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -2,15 +2,5 @@ describe 'Index Page', type: :feature do
   it 'displays home page' do
     visit '/'
   
-      within '.projects' do
-        expect(page).to have_content 'My First Website'
-        expect(page).to have_content 'FizzBuzz'
-      end
-  
-      within '.about_me' do
-        expect(page).to have_content 'American'
-        expect(page).to have_content '06/02/1977'
-        expect(page).to have_content 'Music, Video games, Football, Problem solving'
-      end
   end
 end
