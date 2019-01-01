b628499bb7968c5d2b1e0830072198772cee3714
Jonathan <jon1969edwards@gmail.com>
2019-01-01 15:41:31 +0100

    Updates fetch.rb file and creates json file for api

--- source/stylesheets/fetch/fetch.rb
+++ source/stylesheets/fetch/fetch.rb
@@ -7,9 +7,6 @@ account_sid = 'AC8931c11b7068a2c0a2649fef49af78ae'
 auth_token = '4500ce81c1ac1e2617cd4197840fb620'
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 
-style_sheet = @client.autopilot
-                     .assistants('UA3ad10a9913aab3be8600db571312b270')
-                     .style_sheet
-                     .fetch
+assistant = @client.autopilot.assistants.create
 
 puts style_sheet.assistant_sid
